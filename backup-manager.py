d0844c4e03dc9174a1259e4028b00529719218e4
eddie <eddie.eby@gmail.com>
2026-03-09 22:51:56 -0600

    add error handling to load_devices()

--- backup-manager.py
+++ backup-manager.py
@@ -7,11 +7,16 @@ class BackupManager:
     def load_devices(self):
 
       network_devices = [] # list of NetworkDevice objects
+      json_file_name = "device.json"
       
-      with open("devices.json") as f:
-          devices = json.load(f)
+      try:
+          with open(json_file_name) as f:
+              devices_json_data = json.load(f)
+      except FileNotFoundError:
+          print(f"File name {json_file_name} was not found")
+          return
       
-      for device in devices:
+      for device in devices_json_data:
           device_object = NetworkDevice(device["hostname"], 
                                  device["ip"], 
                                  device["vendor"])
@@ -27,8 +32,8 @@ class BackupManager:
       file_name = f"backups/{device.hostname}" + current_time + ".conf"
       # write config to backups/[hostname][date/time].conf
       with open(file_name, "w") as file:
-         file.write(config)
-         print("Config was saved as backups/" + file_name)
+          file.write(config)
+          print("Config was saved as backups/" + file_name)
       device.disconnect()
       return
 
@@ -37,16 +42,26 @@ class BackupManager:
         
       network_devices = self.load_devices()
 
+      if network_devices is None:
+          print(f"Unable to backup device")
+          return
+
       for device in network_devices:
-         if device.hostname == hostname:
-            self.save_config(device)
-            return
-      error_message = f"No device with hostname '{hostname}' was found"
-      return error_message
+          if device.hostname == hostname:
+              self.save_config(device)
+              return
+
+      print(f"No device with hostname '{hostname}' was found")
       
     def backup_all(self):
         
       network_devices = self.load_devices()
         
-      for device in network_devices:
-          self.save_config(device)
+      if network_devices is not None:
+          for device in network_devices:
+              self.save_config(device)
+          return
+      print("Unable to backup devices")
+
+backup = BackupManager()
+backup.backup_device("R1")
