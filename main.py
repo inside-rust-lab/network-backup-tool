7260234e5c5de0b307cc00e939bbd3f17bca4808
eddie <eddie.eby@gmail.com>
2026-03-10 06:10:01 -0600

    define main.py functionality

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+Needs to accept user credentials for login
+Needs to accept what the user wants to do
+    Backup all devices
+    Backup a particular device
+python main.py backup-all
+python main.py backup R1
+python main.py list-devices
+python main.py show-vendors
+'''
+from backupmanager import BackupManager
+
