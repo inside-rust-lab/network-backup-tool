a3d20afd1079a23c4f601cb7e4f6a72ea8b1089e
eddie <eddie.eby@gmail.com>
2026-03-11 04:46:42 -0600

    netmiko can connect to devices

--- device.py
+++ device.py
@@ -1,6 +1,7 @@
 import time
 import random
 from netmiko import ConnectHandler
+from netmiko import NetmikoTimeoutException, NetmikoAuthenticationException
 
 class NetworkDevice:
     def __init__(self, hostname, host, device_type, username, password, secret):
@@ -10,38 +11,47 @@ class NetworkDevice:
         self.username = username
         self.password = password
         self.secret = secret
+        self.netconnect = None
 
     def connect(self):
         print(f"Attempting to connect to {self.hostname}...")
         connection_established = False
-        connection_attempts = 3
+        connection_attempts = 0
+        max_retries = 3
 
-        while not connection_established and connection_attempts >= 0:
-            random_failure = random.randint(1, 10)
-            if connection_attempts <= 0:
-                print(f"Unable to connect to {self.hostname}. Connection attempts expired")
+        while not connection_established and connection_attempts < max_retries:
+            try:
+                self.net_connect = ConnectHandler(
+                    device_type=self.device_type,
+                    host=self.host,
+                    username=self.username,
+                    password=self.password,
+                    secret=self.secret
+                )
+                print(f"Successfully connected to {self.hostname}")
                 return connection_established
-            if random_failure == 1:
-                connection_attempts -= 1
-                print("Connection failed\nRe-attempting connection...")
-                time.sleep(2)
-            else:
-                print("Connection established")
-                connection_established = True
-                time.sleep(1)
-        return connection_established
+            except NetmikoAuthenticationException:
+                print("Invalid credentials")
+                return connection_established
+            except NetmikoTimeoutException:
+                print(f"Unable to connect to {self.host}")
+                connection_attempts += 1
+            if connection_attempts >= max_retries:
+                print(f"Unable to connect to {self.host} after {max_retries} attempts")
+
 
     def get_config(self):
-        try:
-            with open("configs/" + self.hostname + ".conf", "r") as file:
-                config = file.read()
-                return config
-        except FileNotFoundError:
-            print(f"Unable to retrieve config file file from {self.hostname}")
-            return None
+        if self.net_connect is not None:
+            try:
+                output = self.net_connect.find_prompt()
+                print(output)
+                return output
+            except:
+                print(f"Unable to retrieve config file from {self.hostname}")
+                return None
 
     def disconnect(self):
-        print("Disconnecting...")
-        time.sleep(2)
+        print(f"Disconnecting from {self.hostname}...")
+        self.net_connect.disconnect()
         print("Disconnect successful")
         return
