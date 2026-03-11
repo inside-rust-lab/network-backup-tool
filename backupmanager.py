c6fc01cfbf506adad22495ae12d20a713b732e39
eddie <eddie.eby@gmail.com>
2026-03-10 22:05:39 -0600

    add load_devices to init, proper format for date/time, and error handling to BackupManager

--- backupmanager.py
+++ backupmanager.py
@@ -4,6 +4,8 @@ import os
 from device import NetworkDevice
 
 class BackupManager:
+    def __init__(self):
+        self.devices = self.load_devices()
     
     def load_devices(self):
 
@@ -11,10 +13,16 @@ class BackupManager:
         json_file_name = "devices.json"
         
         try:
-            with open(json_file_name) as f:
-                devices_json_data = json.load(f)
+            with open(json_file_name) as file:
+                devices_json_data = json.load(file)
         except FileNotFoundError:
             print(f"File name {json_file_name} was not found")
+            return
+        except json.JSONDecodeError:
+            print(f"JSON data in {json_file_name} is not formatted properly")
+            return
+        except PermissionError:
+            print(f"You do not have permissions to open {json_file_name}")
             return
         
         for device in devices_json_data:
@@ -36,8 +44,9 @@ class BackupManager:
         if not os.path.isdir(directory):
             os.makedirs(directory)
 
-        current_time = str(datetime.datetime.now())
-        file_name = f"{directory}/{device.hostname}{current_time}.conf"
+        current_time = datetime.datetime.now()
+        current_time_formatted = current_time.strftime("%m-%d-%y_%H:%M:%S")
+        file_name = f"{directory}/{device.hostname}-{current_time_formatted}.conf"
         with open(file_name, "w") as file:
             file.write(config)
             print(f"Config was saved as {directory}/{file_name}")
@@ -47,7 +56,7 @@ class BackupManager:
 
     def backup_device(self, hostname):
         
-        network_devices = self.load_devices()
+        network_devices = self.devices
 
         if network_devices is None:
             print(f"Unable to backup device")
@@ -62,7 +71,7 @@ class BackupManager:
       
     def backup_all(self):
         
-        network_devices = self.load_devices()
+        network_devices = self.devices
         
         if network_devices is not None:
             for device in network_devices:
